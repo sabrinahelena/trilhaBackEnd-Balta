14a649226a3ce646dc4c8d0767d449bb65bc3abf
Sabrina Helena Ferreira <sabrina.ferreira@me.com.br>
2022-09-25 13:25:53 -0300

    projetos sobre generics e lists

---
+++ Aula8Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula8Generics
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+            var context = new DataContext<Person, Payment, Subscription>();
+            context.Save(person);
+            
+        }
+        
+    }
+//Generics. Nosso datacontext salva informações, mas não é preciso fazer um save para cada informação se podemos fazer genérico;
+    public class DataContext<T, U, V> where T : Person// isso significa que T só pode ser do tipo classe person
+    {
+        public void Save(T entity)
+        {
+            
+        }
+
+        public void Save(U entity)
+        {
+            
+        }
+
+        public void Save(V entity)
+        {
+            
+        }
+    }
+
+    public class Person
+    {
+        
+    }
+
+    public class Payment
+    {
+        
+    }
+
+    public class Subscription
+    {
+        
+    }
+}
---
+++ Aula9Listas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aula9Listas
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //IEnumerable<Payment> paymentsB = new List<Payment>(); //lista de leitura.
+            IList<Payment> paymentsA = new List<Payment>(); //Consegue fazer mais coisas com a lista, como adicionar.
+            paymentsA.Add(new Payment(1));
+            paymentsA.Add(new Payment(2));
+            paymentsA.Add(new Payment(3));
+            paymentsA.Add(new Payment(4));
+
+            var paidPaymentos = new List<Payment>();
+            paidPaymentos.AddRange((paymentsA));
+
+            var p = paymentsA.Where(i => i.Id == 1);
+            var pF = paymentsA.FirstOrDefault(x => x.Id == 1);
+
+            var exists = paymentsA.Any(x => x.Id == 2);
+            Console.WriteLine(exists);
+
+            paymentsA.AsEnumerable(); //assim como um enumerable pode ser convertido para tolist, toarray...
+            
+            
+
+
+
+
+
+
+            foreach (var payment in paymentsA)
+            {
+                Console.WriteLine(payment.Id);
+            }
+            
+
+        }
+    }
+
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        public Payment(int id)
+        {
+            Id = id;
+        }
+    }
+}
